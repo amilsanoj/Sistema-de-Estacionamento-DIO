196243af8c5fa76d8dac37d60c7b4a00db0ab564
Jonas Lima Junior <jonasgomeslimajunior@gmail.com>
2023-11-09 14:52:36 -0300

    Projeto Concluído

---
+++ Sistema-de-Estacionamento/Models/Estacionamento.cs
@@ -0,0 +1,131 @@
+﻿namespace Sistema_de_Estacionamento.Models;
+
+public class Estacionamento
+{
+    public Estacionamento(decimal preçoInicial, decimal preçoPorHora)
+    {
+        this.PreçoInicial = preçoInicial;
+        this.PreçoPorHora = preçoPorHora;
+    }
+
+    public decimal PreçoInicial;
+    public decimal PreçoPorHora;
+
+    public List<string> Veiculos = new List<String>();
+
+    public void AdicionarVeiculo()
+    {
+        Console.WriteLine($"Digite a placa do veículo");
+        string placa = Console.ReadLine()!.ToUpper();
+        //Validação quanto a existência do veículo no estacionamento
+        if (Veiculos.Contains(placa))
+        {
+            Console.WriteLine($"{placa} já está no estacionamento");
+        }
+        //Validação da Placa
+        else if(ValidacaoDePlaca(placa))
+        {
+            Veiculos.Add(placa);
+            Console.WriteLine($"Veículo:{placa} adicionado com sucesso");
+        }
+        else
+        {
+            Console.WriteLine("Placa do veículo é inválida");
+        }
+        
+    }
+    
+    public void RemoverVeiculo()
+    {
+        Console.WriteLine($"Digite a placa do veículo");
+        string placa = Console.ReadLine()!.ToUpper();
+        if (Veiculos.Contains(placa))
+        {
+            Console.WriteLine($"Digite o numero de horas que o veículo permaneceu no estacionamento(Hora Inteira, caso " +
+                              $"tenha ficado menos de 1 hora digite 0)");
+            decimal horas = decimal.Parse(Console.ReadLine()!);
+            Console.WriteLine($"Digite o numero de minutos que o veículo permaneceu no estacionamento(Minutos inteiros, " +
+                              $"caso não tenha minutos além de 1 hora digite 0)");
+            decimal minutos = Decimal.Parse(Console.ReadLine()!);
+            if (minutos != 0 || horas != 0)
+            {
+                Console.WriteLine($"O valor a ser pago pela guarda do veículo {placa} por {horas} horas e {minutos} " +
+                                  $"minutos é R${CalcularPreço(horas, minutos)} ");
+                Veiculos.Remove(placa);
+            }
+        }
+        else
+        {
+            Console.WriteLine($"{placa} não está presente na lista de veículos");
+        }
+    }
+
+    public void ListarVeiculos()
+    {
+        if (Veiculos.Any())
+        {
+            Console.WriteLine($"Os veículos estacionados são:");
+            foreach (string placa in Veiculos)
+            {
+                Console.WriteLine($"Placa: {placa}");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Não há veículos estacionados");
+        }
+    }
+    //Calcula o número de horas e minutos e assim retorna o valor total a ser pago. Caso o tempo seja igual ou inferior
+    //a 30 minutos a cobrança será apenas o valor incial, caso seja superior irá cobrar o adicional por minuto.
+    decimal CalcularPreço(decimal horas, decimal minutos)
+    {
+        if (horas == 0 && minutos <= 30)
+        {
+            return PreçoInicial;
+        }
+        return PreçoInicial + PreçoPorHora * ((minutos / 60) + horas);
+        
+    }
+        
+
+    //Validação da placa seguindo o padrão brasileiro
+    bool ValidacaoDePlaca(string placa)
+    {
+        //Valida a quantidade de caracteres
+        bool controle = true;
+        if (placa.Length != 7)
+        {
+            controle = false;
+        }
+        //Valida o conteúdo dos caracteres
+        else
+        {
+            for (int i = 0; i < 7; i++)
+            {
+                if (i > 0 && i < 3 || i == 4)
+                {
+                    if (placa[i] >= 'A' && placa[i] <= 'Z')
+                    {
+                        continue;
+                    }
+
+                    controle = false;
+                    break;
+
+                }
+
+                if (i == 3 || i > 4)
+                {
+                    if (placa[i] >= '0' && placa[i] <= '9')
+                    {
+                        continue;
+                    }
+                    controle = false;
+                    break;
+                }
+            }
+        }
+
+        return controle;
+    }
+}
--- Sistema-de-Estacionamento/Program.cs
+++ Sistema-de-Estacionamento/Program.cs
@@ -1,3 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Sistema_de_Estacionamento.Models;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\n" +
+                  "Digite o preço inicial:");
+ decimal precoInicial = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Agora digite o preço por hora:");
+decimal precoPorHora = Convert.ToDecimal(Console.ReadLine());
+
+// Instancia a classe Estacionamento, já com os valores obtidos anteriormente
+Estacionamento es = new Estacionamento(precoInicial, precoPorHora);
+
+string opcao = string.Empty;
+bool exibirMenu = true;
+
+// Realiza o loop do menu
+while (exibirMenu)
+{
+    Console.Clear();
+    Console.WriteLine("Digite a sua opção:");
+    Console.WriteLine("1 - Cadastrar veículo");
+    Console.WriteLine("2 - Remover veículo");
+    Console.WriteLine("3 - Listar veículos");
+    Console.WriteLine("4 - Encerrar");
+
+    switch (Console.ReadLine())
+    {
+        case "1":
+            es.AdicionarVeiculo();
+            break;
+
+        case "2":
+            es.RemoverVeiculo();
+            break;
+
+        case "3":
+            es.ListarVeiculos();
+            break;
+
+        case "4":
+            exibirMenu = false;
+            break;
+
+        default:
+            Console.WriteLine("Opção inválida");
+            break;
+    }
+
+    Console.WriteLine("Pressione uma tecla para continuar");
+    Console.ReadLine();
+}
+
+Console.WriteLine("O programa se encerrou");
